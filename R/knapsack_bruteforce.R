18ec8a73d8dcaa9f046227ee239988cd12b28219
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-10-08 19:31:47 +0200

    knapsack bruteforce added

--- R/knapsack_bruteforce.R
+++ R/knapsack_bruteforce.R
@@ -1,8 +1,31 @@
-knapsack_bruteforce <- function(data, capacity) {
-  df <- data
-  m <- sapply(as.numeric(data$w), function(x){ as.integer(intToBits(x))})
-  print(m)
+knapsack_bruteforce <- function(x, W, parallel = FALSE) {
+  data <- x
+  lengthOfCombinations <- 2^nrow(df)
+  df <- data.frame()
+  for (i in 1:nrow(data)) {
+    elementCombinations <- combn(rownames(data), i, simplify = FALSE)
+    combinedWeight <- colSums(combn(data$w, i))
+    combinedValue <- colSums(combn(data$v, i))
+    newDF <- data.frame(combinedWeight, combinedValue, I(elementCombinations))
+    df <- rbind(df, newDF)
+  }
+  df <- df[order(df$combinedValue, decreasing = TRUE),]
+  left_weight <- W
+  optimal_value <- 0
+  elemets <- c()
+  for (i in 1:nrow(df)) {
+    row <- df[i,]
+    if (left_weight > row$combinedWeight) {
+      optimal_value <- optimal_value + row$combinedValue
+      left_weight <- left_weight - row$combinedWeight
+      elemets <- c(elemets, row$elementCombinations)
+    }
+  }
+  print(class(row$elementCombinations))
+  return(list("value"=optimal_value, "elements" = elemets))
 }
+
+
 
 
 
--- R/knapsack_greedy.R
+++ R/knapsack_greedy.R
@@ -3,16 +3,16 @@ knapsack_greedy <- function(data, weight) {
   max_values <- data$v / data$w
   df$maxValue <- max_values
   df <- df[order(df$maxValue, decreasing = TRUE),]
-  left_weught <- weight
+  left_weight <- weight
   optimal_value <- 0
-  elemets <- c()
+  elements <- c()
   for (i in 1:nrow(df)) {
     row <- df[i,]
-      if (left_weught > row$w) {
-        optimal_value <- optimal_value + row$v
-        left_weught <- left_weught - row$w
-        elemets <- c(elemets, rownames(row))
-      }
+    if (left_weight > row$w) {
+      optimal_value <- optimal_value + row$v
+      left_weight <- left_weight - row$w
+      elements <- c(elements, rownames(row))
+    }
   }
-  return(list("value"=optimal_value, "elements" = as.numeric(elemets)))
+  return(list("value"=optimal_value, "elements" = as.numeric(elements)))
 }
