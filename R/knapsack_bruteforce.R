637dd9ebcd3614c3b85709d9c9e24d6aa6a88af7
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-10-08 20:09:25 +0200

    brut force added

--- R/knapsack_bruteforce.R
+++ R/knapsack_bruteforce.R
@@ -24,29 +24,3 @@ knapsack_bruteforce <- function(x, W, parallel = FALSE) {
   print(class(row$elementCombinations))
   return(list("value"=optimal_value, "elements" = elemets))
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-RNGversion(min(as.character(getRversion()),"3.5.3"))
-set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
-n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE),
-    v=runif(n = n, 0, 10000)
-  )
-
-knapsack_bruteforce(knapsack_objects[1:8,], 3500)
