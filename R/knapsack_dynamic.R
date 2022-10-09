bf4f8bf5cdfeea6161b3526362994e93203ce9c9
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-10-10 01:25:11 +0200

    dynamic done

--- R/knapsack_dynamic.R
+++ R/knapsack_dynamic.R
@@ -18,17 +18,6 @@ knapsack_dynamic <- function(x, W) {
     mat[n,C] <- res
     return(res)
   }
-  return(k_recursive(n = nrow(data), C = W))
+  return(list(value = k_recursive(n = nrow(data), C = W)))
 }
-
-
-RNGversion(min(as.character(getRversion()),"3.5.3"))
-set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
-n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE),
-    v=runif(n = n, 0, 10000)
-  )
-knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500)
 
