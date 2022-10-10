9caf1a2570b5801c310fca48b34fdd3f2c958b90
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-10-10 15:11:15 +0200

    greedy

--- R/knapsack_greedy.R
+++ R/knapsack_greedy.R
@@ -1,9 +1,10 @@
 greedy_knapsack <- function(x, W) {
   df <- x
+  print(df)
   max_values <- df$v / df$w
   df$maxValue <- max_values
   df <- df[order(df$maxValue, decreasing = TRUE),]
-  left_weight <- weight
+  left_weight <- W
   optimal_value <- 0
   elements <- c()
   for (i in 1:nrow(df)) {
@@ -14,5 +15,10 @@ greedy_knapsack <- function(x, W) {
       elements <- c(elements, rownames(row))
     }
   }
-  return(list("value"=optimal_value, "elements" = as.numeric(elements)))
+  numericElements <- as.numeric(elements)
+  tail <- tail(numericElements, n=1)
+  lastIndex <- length(numericElements)
+  optimal_value <- as.numeric(optimal_value) - as.numeric(x$v[tail])
+  return(list("value"=round(optimal_value), "elements" = numericElements[-35]))
 }
+
