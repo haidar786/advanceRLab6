cf39c89397d6884882896b96916879521a4efd84
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-10-10 15:21:42 +0200

    brute force improved

--- R/knapsack_bruteforce.R
+++ R/knapsack_bruteforce.R
@@ -18,21 +18,8 @@ brute_force_knapsack <- function(x, W, ...) {
     if (left_weight > row$combinedWeight) {
       optimal_value <- optimal_value + row$combinedValue
       left_weight <- left_weight - row$combinedWeight
-      elemets <- c(elemets, row$elementCombinations)
+      elemets <- c(elemets, unlist(row$elementCombinations))
     }
   }
-  print(class(row$elementCombinations))
-  return(list("value"=optimal_value, "elements" = elemets))
+  return(list("value"=round(optimal_value), "elements" = as.numeric(elemets)))
 }
-
-
-RNGversion(min(as.character(getRversion()),"3.5.3"))
-set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
-n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE),
-    v=runif(n = n, 0, 10000)
-  )
-
-brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
