385ecc49a4aff0271edb289e685810da1748f8aa
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-10-11 21:34:57 +0200

    greedy problem solved test pass

--- R/knapsack_greedy.R
+++ R/knapsack_greedy.R
@@ -1,23 +1,28 @@
 greedy_knapsack <- function(x, W) {
+  stopifnot(is.data.frame(x))
+  stopifnot(W > 0)
   df <- x
   max_values <- df$v / df$w
   df$maxValue <- max_values
+  rn <- rownames(df)
+  df$elements <- rn
   df <- df[order(df$maxValue, decreasing = TRUE),]
+  rownames(df) <- rn
   left_weight <- W
   optimal_value <- 0
   elements <- c()
-  for (i in 1:nrow(df)) {
-    row <- df[i,]
-    if (left_weight > row$w) {
-      optimal_value <- optimal_value + row$v
-      left_weight <- left_weight - row$w
-      elements <- c(elements, rownames(row))
+  index <- 1
+  repeat {
+    if (left_weight >= df$w[index]) {
+      optimal_value <- optimal_value + df$v[index]
+      left_weight <- left_weight - df$w[index]
+      elements <- c(elements, df$elements[index])
+    }else {
+      break
     }
+    index <- index + 1
   }
-  numericElements <- as.numeric(elements)
-  tail <- tail(numericElements, n=1)
-  lastIndex <- length(numericElements)
-  optimal_value <- as.numeric(optimal_value) - as.numeric(x$v[tail])
-  return(list("value"=round(optimal_value), "elements" = numericElements[-35]))
+  return(list("value"=round(optimal_value), "elements" = as.numeric(elements)))
 }
+
 
