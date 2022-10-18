e51686f4a7a94c2b44415f28afabcdcbbf3826ee
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-10-18 03:22:38 +0200

    clear

--- R/knapsack_bruteforce.R
+++ R/knapsack_bruteforce.R
@@ -62,13 +62,3 @@ brute_force_knapsack <- function(x, W, parallel = FALSE) {
   }
   return(list("value"=round(optimal_value), "elements" = as.numeric(elemets)))
 }
-
-RNGversion(min(as.character(getRversion()),"3.5.3"))
-set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
-n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE),
-    v=runif(n = n, 0, 10000)
-  )
-
